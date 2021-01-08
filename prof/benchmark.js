d4b0671c58fcfbe8ca71758bdff1f4e1f5f73df9
Kiril <ikera008@gmail.com>
2021-01-08 15:06:09 +0200

    remove webstorm files

---
+++ prof/benchmark.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const autocannon = require('autocannon')
+
+autocannon({
+    url: 'http://localhost:5000',
+    connections: 10,
+    pipelining: 1,
+    duration: 10
+}, console.log)
